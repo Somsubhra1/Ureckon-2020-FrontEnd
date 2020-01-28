7eb2c890542eb309cfbd8f1d19e8132ca5cc8769
Somsubhra Das <das.somsubhra1@gmail.com>
2020-01-28 23:11:04 +0530

    Added social icons to homepage

--- js/index.js
+++ js/index.js
@@ -290,3 +290,27 @@ var buildHex = function(size, radius) {
         }
     }
 };
+
+
+$(document).ready(function () {
+
+    if ($(document).width() <= 800) {
+        $('.s-media').addClass('sm-collapse');
+        $('#sm-open').delay(300).css('left', '0');
+
+    }
+    
+    // Social Media Collapse
+    $('#sm-close').click(function () {
+        $('.s-media').addClass('sm-collapse');
+        $('#sm-open').delay(300).css('left', '0');
+    });
+
+    $('#sm-open').click(function () {
+        $('#sm-open').css('left', '-60px');
+        $('.s-media').removeClass('sm-collapse');
+
+    });
+
+    // END Click Function
+});// END DOCUMENT.Ready
